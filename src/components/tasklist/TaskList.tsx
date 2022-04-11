1254a3450fbff1dce395643b13fb0668592bd070
Denis OSullivan <deniskos@hotmail.com>
2022-04-11 13:19:58 -0700

    also built version of columns using flexbox and some other layout tweaks

--- src/components/tasklist/TaskList.tsx
+++ src/components/tasklist/TaskList.tsx
@@ -51,104 +51,148 @@ const isDone = (task: any) => task.state === 'done';
 export default function TaskList() {
   return (
     <div className="notion-frame">
-      <div className="notion-scroller">
+      <div className="notion-box">
 
-        <div className="notion-header-container">
+        <div className="notion-scroller">
 
-          <div className="notion-header-container-title">
-            <div className="notion-record-icon notion-focusable" role="button" aria-disabled="false" tabIndex={0}>
-              <div className="icon-image-container">
-                <span className="icon-image" role="image" aria-label="✔️">✔️</span>
+          <div className="notion-header-container">
+
+            <div className="notion-header-container-title">
+              <div className="notion-record-icon notion-focusable" role="button" aria-disabled="false" tabIndex={0}>
+                <div className="icon-image-container">
+                  <span className="icon-image" role="image" aria-label="✔️">✔️</span>
+                </div>
+              </div>
+
+              <div className="title-container">
+                <div className="title">Task List</div>
               </div>
             </div>
 
-            <div className="title-container">
-              <div className="title">Task List</div>
+            <div className="header-body-text">
+              <div>
+              Use this template to track your personal tasks. 
+              Click <span className="text-action-link">New</span>
+              </div>
+              to create a new task directly on this board.
+              Click an existing task to add additional context or subtasks.
+
             </div>
           </div>
 
-          <div className="header-body-text">
-            <div>
-            Use this template to track your personal tasks. 
-            Click <span className="text-action-link">New</span>
+          <div className="menu-bar">
+            <div className="view-choices">
+              <div className="view-choice">
+                <span className="icon"></span>
+                <div className="view-text">Board View</div>
+              </div>
+              <div className="view-choice">
+                <span className="icon"></span>
+                <div className="view-text">Table View</div>
+              </div>
             </div>
-            to create a new task directly on this board.
-            Click an existing task to add additional context or subtasks.
+
+            <div className="data-choices">
+              <div className="data-choice">
+                <button className="view-text">Filter</button>
+                <button className="view-text">Sort</button>
+
+                <button className="view-text">Search</button>
+                <button className="view-text">...</button>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="notion-main-tasks-container">
+            <div className="notion-main-tasks-column-list">
+              <div className="task-title">
+                To-dos
+              </div>
+
+              {taskData.filter(isToDo).map((task) => (
+                <a className="task-card" key={task.id} href="/#">
+                {task.title}
+                </a>
+              ))}
+
+              <a className="task-add" href="/#">
+                + New
+              </a>
+            </div>
+
+            <div className="notion-main-tasks-column-list">
+              <div className="task-title">
+                In Progress
+              </div>
+
+              {taskData.filter(isInProgress).map((task) => (
+                <a className="task-card" key={task.id} href="/#">
+                {task.title}
+                </a>
+              ))}
+
+              <a className="task-add" href="/#">
+                + New
+              </a>
+            </div>
+
+            <div className="notion-main-tasks-column-list">
+              <div className="task-title">
+                Done
+              </div>
+
+              {taskData.filter(isDone).map((task) => (
+                <a className="task-card" key={task.id} href="/#">
+                {task.title}
+                </a>
+              ))}
+            </div>
 
           </div>
         </div>
 
-        <div className="menu-bar">
-          <div className="view-choices">
-            <div className="view-choice">
-              <span className="icon"></span>
-              <div className="view-text">Board View</div>
-            </div>
-            <div className="view-choice">
-              <span className="icon"></span>
-              <div className="view-text">Table View</div>
-            </div>
-          </div>
 
-          <div className="data-choices">
-            <div className="data-choice">
-              <button className="view-text">Filter</button>
-              <button className="view-text">Sort</button>
+        
+        <div className="flex-example-lists-container">
+          <h4>Next, same column layout using flexbox</h4>
 
-              <button className="view-text">Search</button>
-              <button className="view-text">...</button>
-            </div>
+          <div className="lists-container">
+            <div className="list-list">
+              <div className="task-title">
+                To-dos
+              </div>
 
-          </div>
-        </div>
+              {taskData.filter(isToDo).map((task) => (
+                <a className="task-card" key={task.id} href="/#">
+                {task.title}
+                </a>
+              ))}
 
-
-        <div className="notion-main-tasks-container">
-          <div className="notion-main-tasks-column-list">
-            <div className="task-title">
-              To-dos
-            </div>
-
-            {taskData.filter(isToDo).map((task) => (
-              <a className="task-card" key={task.id} href="/#">
-              {task.title}
+              <a className="task-add" href="/#">
+                + New
               </a>
-            ))}
-
-            <a className="task-add" href="/#">
-              + New
-            </a>
-          </div>
-
-          <div className="notion-main-tasks-column-list">
-            <div className="task-title">
-              In Progress
             </div>
 
-            {taskData.filter(isInProgress).map((task) => (
-              <a className="task-card" key={task.id} href="/#">
-              {task.title}
+            <div className="list-list">
+              <div className="task-title">
+                In Progress
+              </div>
+
+              {taskData.filter(isInProgress).map((task) => (
+                <a className="task-card" key={task.id} href="/#">
+                {task.title}
+                </a>
+              ))}
+
+              <a className="task-add" href="/#">
+                + New
               </a>
-            ))}
-
-            <a className="task-add" href="/#">
-              + New
-            </a>
-          </div>
-
-          <div className="notion-main-tasks-column-list">
-            <div className="task-title">
-              Done
             </div>
-
-            {taskData.filter(isDone).map((task) => (
-              <a className="task-card" key={task.id} href="/#">
-              {task.title}
-              </a>
-            ))}
           </div>
 
         </div>
+
       </div>
     </div>
   );
